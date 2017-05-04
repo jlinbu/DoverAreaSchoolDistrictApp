bf16e67a152700b87063bd0a90a510d59e105056
ExpandingDev <javscripter@yahoo.com>
2017-05-04 09:25:28 -0400

    Added in Event class. Created lists to hold Events, added in ListView and ListAdapter. Added in OnDateChangeListener for the Calendar View.

---
+++ app/src/main/java/com/example/emailey/doverareaschooldistrictapp/Event.java
@@ -0,0 +1,44 @@
+package com.example.emailey.doverareaschooldistrictapp;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by tsengia on 5/4/2017.
+ */
+
+class Event {
+    private String title = "Default Name";
+    private Date date = Calendar.getInstance().getTime();
+    private String description = "This is an example description for a calendar event.";
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Event(String title, Date date) {
+        this.title = title;
+        this.date = date;
+    }
+
+}
