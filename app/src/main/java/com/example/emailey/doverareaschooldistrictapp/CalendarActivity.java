8b8603ed6532d44b35a5215f70effa2b08293391
ExpandingDev <javscripter@yahoo.com>
2017-05-04 08:59:30 -0400

    Added in dimension values. Created layout file for event bubble. Added in views for the Calendar Activity.

--- app/src/main/java/com/example/emailey/doverareaschooldistrictapp/CalendarActivity.java
+++ app/src/main/java/com/example/emailey/doverareaschooldistrictapp/CalendarActivity.java
@@ -2,12 +2,20 @@ package com.example.emailey.doverareaschooldistrictapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+
+import java.util.Calendar;
 
 public class CalendarActivity extends AppCompatActivity {
+
+    private CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView); // Get the calendar view
+        calendarView.setDate(Calendar.getInstance().getTime().getTime()); // Set the calendar view to today's date
     }
 }
