fb60487f0b05ea94e585dce8ada45bd7c31014d6
ExpandingDev <javscripter@yahoo.com>
2017-05-08 08:38:41 -0400

    Made events visible in the CalendarFragment.

--- app/src/main/java/com/example/emailey/doverareaschooldistrictapp/CalendarFragment.java
+++ app/src/main/java/com/example/emailey/doverareaschooldistrictapp/CalendarFragment.java
@@ -4,10 +4,13 @@ import android.app.Fragment;
 import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.BaseAdapter;
 import android.widget.CalendarView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -30,6 +33,7 @@ public class CalendarFragment extends Fragment {
 
     private CalendarView calendarView;
     private ListView eventsListView;
+    private EventAdapter adapter;
 
     private List<Event> eventsList = new ArrayList<Event>(); // This list holds all of the Calendar events
     private List<Event> currentEventsList = new ArrayList<Event>(); // This list holds all of the Calendar events for the currently selected date
@@ -37,8 +41,19 @@ public class CalendarFragment extends Fragment {
     private Calendar calendar = Calendar.getInstance();
     public final static SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMdd");
 
-    private class onCalendarDateChanged implements CalendarView.OnDateChangeListener {
+    public void checkForEvents() {
+        currentEventsList.clear();
+        String date = DATE_FORMAT.format(selectedDate);
 
+        for(Event e : eventsList) {
+            if(date.equals(DATE_FORMAT.format(e.getDate()))) {
+                currentEventsList.add(e);
+            }
+        }
+        eventsListView.invalidateViews();
+    }
+
+    private class onCalendarDateChanged implements CalendarView.OnDateChangeListener {
         @Override
         public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) { // This is called when the user selects a new date
             Calendar c = Calendar.getInstance(); // Here we are
@@ -46,34 +61,12 @@ public class CalendarFragment extends Fragment {
             c.set(Calendar.MONTH, month);
             c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
             selectedDate = c.getTime();
-            Log.i("WASERDTF", DATE_FORMAT.format(selectedDate));
-
-            currentEventsList.clear();
-            String date = DATE_FORMAT.format(selectedDate);
-
-            for(Event e : eventsList) {
-                if(date.equals(DATE_FORMAT.format(e.getDate()))) {
-                    Log.i("BORG", e.getTitle());
-                    currentEventsList.add(e);
-                }
-            }
-            eventsListView.invalidate();
+            checkForEvents();
         }
     }
 
-    @Override
-    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.activity_calendar, container, false);
-
-        eventsList.add(new Event("Testing", Calendar.getInstance().getTime()));
-
-        calendarView = (CalendarView) root.findViewById(R.id.calendarView); // Get the calendar view
-        calendarView.setDate(Calendar.getInstance().getTime().getTime()); // Set the calendar view to today's date
-
-        calendarView.setOnDateChangeListener(new CalendarFragment.onCalendarDateChanged()); // Set the listener for when the user picks a new date
-        eventsListView = (ListView) root.findViewById(R.id.events_list); // Get the list view for the Events
-
-        eventsListView.setAdapter(new ListAdapter() { // Setting the list adapter for the Events List
+    private class EventAdapter extends BaseAdapter implements ListAdapter { // Setting the list adapter for the Events List
+        private LayoutInflater inflater;
             @Override
             public boolean areAllItemsEnabled() {
                 return false;
@@ -118,7 +111,6 @@ public class CalendarFragment extends Fragment {
             public View getView(int position, View convertView, ViewGroup parent) {
                 View root = inflater.inflate(R.layout.event_bubble, parent, false);
                 ((TextView) root.findViewById(R.id.event_bubble_title)).setText(currentEventsList.get(position).getTitle());
-                Log.i("RARG", "GETVIEW");
                 return root;
             }
 
@@ -136,7 +128,26 @@ public class CalendarFragment extends Fragment {
             public boolean isEmpty() {
                 return currentEventsList.size() == 0;
             }
-        });
+
+            public EventAdapter(LayoutInflater inf) {
+                this.inflater = inf;
+            }
+    }
+
+    @Override
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.activity_calendar, container, false);
+
+        eventsList.add(new Event("Testing", Calendar.getInstance().getTime())); // This is a sample event that is set to the current date.
+
+        calendarView = (CalendarView) root.findViewById(R.id.calendarView); // Get the calendar view
+        calendarView.setDate(Calendar.getInstance().getTime().getTime()); // Set the calendar view to today's date
+
+        calendarView.setOnDateChangeListener(new CalendarFragment.onCalendarDateChanged()); // Set the listener for when the user picks a new date
+        eventsListView = (ListView) root.findViewById(R.id.events_list); // Get the list view for the Events
+        adapter = new EventAdapter(inflater);
+        eventsListView.setAdapter(adapter);
+        checkForEvents();
         return root;
     }
 }
