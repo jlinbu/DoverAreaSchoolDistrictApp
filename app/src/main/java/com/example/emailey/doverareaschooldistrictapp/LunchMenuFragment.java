753b4ce542dd9ef2cdaceeca9856b1dee142f33d
EMailey <superginger65@gmail.com>
2017-05-12 09:09:17 -0400

    Menu and Lunch menu tabs now work, UI not completely done.

--- app/src/main/java/com/example/emailey/doverareaschooldistrictapp/LunchMenuFragment.java
+++ app/src/main/java/com/example/emailey/doverareaschooldistrictapp/LunchMenuFragment.java
@@ -15,22 +15,11 @@ import android.webkit.WebViewClient;
 
 public class LunchMenuFragment extends Fragment {
 
-    public WebView LunchMenuWebView;
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.lunch_menu_fragment, container, false);
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View Lunch = inflater.inflate(R.layout.lunch_menu_fragment, container, false);
-        LunchMenuWebView = (WebView) Lunch.findViewById(R.id.lunchmenuwebview);
-        LunchMenuWebView.loadUrl("http://www.doversd.org/downloads/food_service_and_transportation/may_912_lunch_menu.pdf");
-
-        // Enable Javascript
-        WebSettings webSettings = LunchMenuWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-
-        // Force links and redirects to open in the WebView instead of in a browser
-        LunchMenuWebView.setWebViewClient(new WebViewClient());
-
-        return Lunch;
-
+        return root;
     }
+
 }
 
