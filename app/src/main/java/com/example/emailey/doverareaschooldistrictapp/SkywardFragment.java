74339772a312cc91de5491abba4c68c18a212b4e
Bmotter <ravenspwnu@gmail.com>
2017-05-17 08:47:44 -0400

    Added functionality to the Skyward tab.

---
+++ app/src/main/java/com/example/emailey/doverareaschooldistrictapp/SkywardFragment.java
@@ -0,0 +1,40 @@
+package com.example.emailey.doverareaschooldistrictapp;
+
+import android.app.Activity;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebViewFragment;
+import android.widget.Toast;
+
+/**
+ * Created by Bmotter on 5/8/2017.
+ */
+
+public class SkywardFragment extends Fragment {
+    public WebView fSkywardWebView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.skyward_fragment, container, false);
+        fSkywardWebView = (WebView) v.findViewById(R.id.fskywardwebview);
+        fSkywardWebView.loadUrl("https://skyward.iscorp.com/scripts/wsisa.dll/WService=wsedudoverpa/seplog01.w");
+
+        // Enable Javascript
+        WebSettings webSettings = fSkywardWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        fSkywardWebView.setWebViewClient(new WebViewClient());
+
+        return v;
+
+    }
+}
