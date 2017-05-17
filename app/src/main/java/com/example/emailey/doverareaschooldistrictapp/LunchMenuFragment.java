f1abff25a3d65e5dfe2cd92ba282b11787b3e7a0
Bmotter <ravenspwnu@gmail.com>
2017-05-17 09:12:43 -0400

    Fixing lunch menu tab (WIP)

--- app/src/main/java/com/example/emailey/doverareaschooldistrictapp/LunchMenuFragment.java
+++ app/src/main/java/com/example/emailey/doverareaschooldistrictapp/LunchMenuFragment.java
@@ -1,25 +1,41 @@
 package com.example.emailey.doverareaschooldistrictapp;
 
+import android.app.Activity;
+
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.webkit.WebViewFragment;
+import android.widget.Toast;
 
 /**
- * Created by EMailey on 5/11/2017.
+ * Created by Bmotter on 5/8/2017.
  */
 
 public class LunchMenuFragment extends Fragment {
+    public WebView fLunchWebView;
 
-    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.lunch_menu_fragment, container, false);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.lunch_menu_fragment, container, false);
+        fLunchWebView = (WebView) v.findViewById(R.id.flunchmenuwebview);
+        fLunchWebView.loadUrl("http://www.doversd.org/downloads/food_service_and_transportation/may_912_lunch_menu.pdf");
 
-        return root;
+        // Enable Javascript
+        WebSettings webSettings = fLunchWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        fLunchWebView.setWebViewClient(new WebViewClient());
+
+        return v;
+
     }
-
 }
 
