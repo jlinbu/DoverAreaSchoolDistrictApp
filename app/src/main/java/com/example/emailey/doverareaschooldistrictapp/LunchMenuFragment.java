685c3767cc7b3f9d6787c93586e248311a06bbfb
EMailey <superginger65@gmail.com>
2017-05-18 08:33:48 -0400

    Fixed Braydens trash coding

--- app/src/main/java/com/example/emailey/doverareaschooldistrictapp/LunchMenuFragment.java
+++ app/src/main/java/com/example/emailey/doverareaschooldistrictapp/LunchMenuFragment.java
@@ -19,23 +19,12 @@ import android.widget.Toast;
  */
 
 public class LunchMenuFragment extends Fragment {
-    public WebView fLunchWebView;
 
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.lunch_menu_fragment, container, false);
-        fLunchWebView = (WebView) v.findViewById(R.id.flunchmenuwebview);
-        fLunchWebView.loadUrl("http://www.doversd.org/parentsstudents/");
 
-        // Enable Javascript
-        WebSettings webSettings = fLunchWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-
-        // Force links and redirects to open in the WebView instead of in a browser
-        fLunchWebView.setWebViewClient(new WebViewClient());
 
         return v;
-
-    }
+}
 }
 
