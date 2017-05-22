9326039763745a282ff0e3c02ae5c37429c28eea
Bmotter <ravenspwnu@gmail.com>
2017-05-22 08:16:26 -0400

    Removed unused import statements in the WebView Fragments

--- app/src/main/java/com/example/emailey/doverareaschooldistrictapp/ParentsStudentsFragment.java
+++ app/src/main/java/com/example/emailey/doverareaschooldistrictapp/ParentsStudentsFragment.java
@@ -1,19 +1,15 @@
 
 package com.example.emailey.doverareaschooldistrictapp;
 
-import android.app.Activity;
 
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.webkit.WebViewFragment;
-import android.widget.Toast;
 
 /**
  * Created by Bmotter on 5/8/2017.
